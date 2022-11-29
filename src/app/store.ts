db99d1641386be236f011fe3d372d484c7be5738
MaxManis <maxbovtun@gmail.com>
2022-11-29 22:46:16 +0200

    base setup 01

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+// import Reducer from '../features/Slice';
+
+export const store = configureStore({
+    reducer: {
+        // goods: Reducer,
+    },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+/* eslint-disable @typescript-eslint/indent */
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+    >;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {store} from "./app/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
