18609fc2116bfabf5b33117abbb4e30f81cc0e56
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-12-01 00:27:53 +0200

    implemeted grid

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,6 +2,41 @@ import React from 'react';
 
 export const HomePage: React.FC = () => {
   return (
-    <div>Home Page</div>
+    <div className="home-page">
+      <section className="page_section welcome">
+        <div className="grid grid--desktop">
+          <div
+            className="grid__item
+            grid__item--desktop-1-17
+            grid__item--tablet-1-9"
+          >
+            <div className="welcome__title">Welcome to Nice Gadgets store!</div>
+          </div>
+          <div
+            className="grid__item
+            grid__item--desktop-1-1
+            grid__item--tablet-1-1"
+          >
+            <div className="welcome__left-sidebar"></div>
+          </div>
+          <div
+            className="grid__item
+            grid__item--desktop-2-23
+            grid__item--tablet-2-11"
+          >
+            <div className="welcome__now-available"></div>
+          </div>
+          <div
+            className="
+            grid__item
+            grid__item--desktop-24-24
+            grid__item--tablet-12-12
+            "
+          >
+            <div className="welcome__right-sidebar"></div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
